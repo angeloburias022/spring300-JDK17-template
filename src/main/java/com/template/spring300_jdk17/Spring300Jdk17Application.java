3f7c753c77397e831412b5dc1fa5e7ff7d39cb39
lou <angelo@solutionsresource.com>
2024-11-01 23:09:15 +0800

    [ADD][INITIAL] Added initial setup dependencies for springboot 3.0.0 JDK 17 project

---
+++ src/main/java/com/template/spring300_jdk17/Spring300Jdk17Application.java
@@ -0,0 +1,13 @@
+package com.template.spring300_jdk17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring300Jdk17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring300Jdk17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/template/spring300_jdk17/Spring300Jdk17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.spring300_jdk17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring300Jdk17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
